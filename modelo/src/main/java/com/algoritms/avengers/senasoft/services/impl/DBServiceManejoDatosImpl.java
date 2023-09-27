7bc09aad9fe28e3c3ab7e039274f770d590ee4e2
Angel Corzo <josedanielmmf@gmail.com>
2023-09-27 08:30:46 -0500

    Añadida la funcion de guardar datos en una nueva implementacion llamada DBServiceManejoDatosImpl

--- modelo/src/main/java/com/algoritms/avengers/senasoft/services/impl/DBServiceManejoDatosImpl.java
+++ modelo/src/main/java/com/algoritms/avengers/senasoft/services/impl/DBServiceManejoDatosImpl.java
@@ -1,5 +1,9 @@
 package com.algoritms.avengers.senasoft.services.impl;
 
+import com.algoritms.avengers.senasoft.model.MotivosDesercion;
+import com.algoritms.avengers.senasoft.model.Recursos;
+import com.algoritms.avengers.senasoft.model.Sector;
+import com.algoritms.avengers.senasoft.model.Usuarios;
 import com.algoritms.avengers.senasoft.repository.MotivosRepository;
 import com.algoritms.avengers.senasoft.repository.RecursosRepository;
 import com.algoritms.avengers.senasoft.repository.SectorRepository;
@@ -7,6 +11,8 @@ import com.algoritms.avengers.senasoft.repository.UsuarioRepository;
 import com.algoritms.avengers.senasoft.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("manejoDatos")
 public class DBServiceManejoDatosImpl implements DBService {
@@ -21,5 +27,21 @@ public class DBServiceManejoDatosImpl implements DBService {
         this.sectorRepository = sectorRepository;
         this.recursosRepository = recursosRepository;
         this.motivosRepository = motivosRepository;
+    }
+
+    public void guardarUsuario(Usuarios usuario){
+        usuarioRepository.save(usuario);
+    }
+
+    public void guardarSector(Sector sector){
+        sectorRepository.save(sector);
+    }
+
+    public void guardarRecursos(Recursos recurso){
+        recursosRepository.save(recurso);
+    }
+
+    public void guardarMotivo(MotivosDesercion motivo){
+        motivosRepository.save(motivo);
     }
 }
