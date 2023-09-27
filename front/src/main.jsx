69f2f76a057cb0af2b17f31be4050c89a54115c4
JuanSeGA <117958247+JuanSeGArtor@users.noreply.github.com>
2023-09-27 08:09:49 -0500

    aplicar clean architecture a estructura del proyecto

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./pages/home/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
