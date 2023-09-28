0ce9f6f2004a5cb737541042e9646593ccf51a8e
DavidPineda25 <davidbpineda251@gmail.com>
2023-09-28 09:49:59 -0500

    Eliminación de una importanción inutilizable

--- modelo/src/main/java/com/algoritms/avengers/senasoft/SenasoftApplication.java
+++ modelo/src/main/java/com/algoritms/avengers/senasoft/SenasoftApplication.java
@@ -2,7 +2,7 @@ package com.algoritms.avengers.senasoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 
 @SpringBootApplication
